2d3bf39f67d1eb9f1b938a5c1870122a7aacf852
Pete R Jemian <prjemian@users.noreply.github.com>
2017-03-28 17:35:00 -0500

    Update simple_bluesky_demo.py

--- demos/simple_bluesky_demo.py
+++ demos/simple_bluesky_demo.py
@@ -11,8 +11,10 @@ then execute these lines from the terminal
 import epics
 
 IOC = 'prj:'
-epics.caput(IOC+'userCalc1.CALC', 'rndm')
-epics.caput(IOC+'userCalc1.SCAN', '.2 second')
+CALC = IOC+'userCalc1'
+
+epics.caput(CALC+'.CALC', 'rndm')
+epics.caput(CALC+'.SCAN', '.2 second')
 epics.caput(IOC+'EnableUserCalcs', 1)
 
 
@@ -29,17 +31,16 @@ history
 
 
 # setup "our detector" that is configured in ophyd as "noisy"
-epics.caput(IOC+'userCalc1.INAN', IOC+'m1.RBV')
-epics.caput(IOC+'userCalc1.INBN', IOC+'m2.RBV')
-epics.caput(IOC+'userCalc1.C', '0.05')
-epics.caput(IOC+'userCalc1.D', '10')
+epics.caput(CALC+'.INAN', IOC+'m1.RBV')
+epics.caput(CALC+'.INBN', IOC+'m2.RBV')
+epics.caput(CALC+'.C', '0.05')
+epics.caput(CALC+'.D', '10')
 # simple noisy signal, proportional to motor m1
-epics.caput(IOC+'userCalc1.CALC', 'A+C*(rndm-0.5)')
-epics.caput(IOC+'userCalc1.SCAN', '.2 second')
 epics.caput(IOC+'EnableUserCalcs', '1')
-epics.caput(IOC+'userCalc1.CALC', 'D*cos(A+B+C*rndm)^2')
+epics.caput(CALC+'.SCAN', '.2 second')
+epics.caput(CALC+'.CALC', 'D*cos(A+B+C*rndm)^2')
 RE(
     outer_product_scan([noisy], m1, -.5, .5, 10, m2, -.7, .7, 10, True), 
     LiveMesh('m1', 'm2', 'noisy', xlim=(-.5,.5), ylim=(-.7,.7), clim=(0, 10)),
-    calc=epics.caget("prj:userCalc1.CALC")
+    calc=epics.caget(CALC+".CALC")
 )
