7d27b57dc993659c7bdcb03f8332ebac3aa1ec9d
Pete Jemian <prjemian@gmail.com>
2017-04-10 13:52:45 -0500

    add the XXX scaler

--- setup/ipython_startups/mongodb/20-detectors.py
+++ setup/ipython_startups/mongodb/20-detectors.py
@@ -7,3 +7,4 @@ import time
 ## Beam Monitor Counts
 #bs_bm2 = EpicsSignalRO('BL14B:Det:BM2', name='bs_bm2')
 noisy = EpicsSignalRO('xxx:userCalc1', name='noisy')
+scaler = EpicsScaler('xxx:scaler1', name='scaler')
