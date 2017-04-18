a7111160f56ccd4d85561ed7192b51bd4511c540
Pete Jemian <prjemian@gmail.com>
2017-04-18 10:57:20 -0500

    refs #5 code cleanup

--- plans/standlone.py
+++ plans/standlone.py
@@ -15,6 +15,7 @@ SYNAPPS_IOC_PREFIX = 'xxx:'
 
 # these things must import and set first, then other imports
 import os
+import sys
 os.environ['EPICS_CA_MAX_ARRAY_BYTES'] = str(EPICS_CA_MAX_ARRAY_BYTES)
 
 import epics
@@ -79,7 +80,6 @@ import suitcase.nexus
 #############################################################################
 
 
-# this *should* come from ~/.config/filestore and ~/.config/metadatastore
 os.environ['MDS_HOST'] = MONGODB_HOST
 os.environ['MDS_PORT'] = '27017'
 os.environ['MDS_DATABASE'] = 'metadatastore-production-v1'
@@ -96,42 +96,25 @@ mds_config = {'host': os.environ['MDS_HOST'],
 fs_config = {'host': os.environ['FS_HOST'],
              'port': int(os.environ['FS_PORT']),
              'database': os.environ['FS_DATABASE']}
+
 mds = MDS(mds_config)
-# For code that only reads the databases, use the readonly version
-#mds_readonly = MDSRO(mds_config)
-#fs_readonly = FileStoreRO(fs_config)
 fs = FileStore(fs_config)
 db = Broker(mds, fs)
 
-# Subscribe metadatastore to documents.
-# If this is removed, data is not saved to metadatastore.
 RE = gs.RE  # convenience alias
 RE.subscribe('all', mds.insert)
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# setup the movables
 
-alpha = EpicsMotor('xxx:m1', name='alpha')
-beta = EpicsMotor('xxx:m2', name='beta')
-gamma = EpicsMotor('xxx:m3', name='gamma')
-x = EpicsMotor('xxx:m4', name='x')
-y = EpicsMotor('xxx:m5', name='y')
-z = EpicsMotor('xxx:m6', name='z')
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# setup the detectors
+alpha = EpicsMotor(SYNAPPS_IOC_PREFIX + 'm1', name='alpha')
+beta = EpicsMotor(SYNAPPS_IOC_PREFIX + 'm2', name='beta')
+gamma = EpicsMotor(SYNAPPS_IOC_PREFIX + 'm3', name='gamma')
+x = EpicsMotor(SYNAPPS_IOC_PREFIX + 'm4', name='x')
+y = EpicsMotor(SYNAPPS_IOC_PREFIX + 'm5', name='y')
+z = EpicsMotor(SYNAPPS_IOC_PREFIX + 'm6', name='z')
 
-## Beam Monitor Counts
-#bs_bm2 = EpicsSignalRO('BL14B:Det:BM2', name='bs_bm2')
-noisy = EpicsSignalRO('xxx:userCalc1', name='noisy')
-scaler = EpicsScaler('xxx:scaler1', name='scaler')
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+noisy = EpicsSignalRO(SYNAPPS_IOC_PREFIX + 'userCalc1', name='noisy')
+scaler = EpicsScaler(SYNAPPS_IOC_PREFIX + 'scaler1', name='scaler')
 
-# class HDF5PluginWithFileStore(HDF5Plugin, FileStoreHDF5IterativeWrite):
-# 
-#     def get_frames_per_point(self):
-#         # return self.num_capture.get()
-#         return 1
 
 # class myHDF5Plugin(HDF5Plugin):
 #  
@@ -152,51 +135,39 @@ class MyDetector(SingleTrigger, SimDetector):
 
     no_op = None
     # image1 = Cpt(ImagePlugin, 'image1:')
-    # stats1 = Cpt(StatsPlugin, 'Stats1:')
-    # stats2 = Cpt(StatsPlugin, 'Stats2:')
-    # stats3 = Cpt(StatsPlugin, 'Stats3:')
-    # stats4 = Cpt(StatsPlugin, 'Stats4:')
-    # stats5 = Cpt(StatsPlugin, 'Stats5:')
-    # trans1 = Cpt(TransformPlugin, 'Trans1:')
-    # roi1 = Cpt(ROIPlugin, 'ROI1:')
-    # roi2 = Cpt(ROIPlugin, 'ROI2:')
-    # roi3 = Cpt(ROIPlugin, 'ROI3:')
-    # roi4 = Cpt(ROIPlugin, 'ROI4:')
-    # proc1 = Cpt(ProcessPlugin, 'Proc1:')
-#     hdf1 = Cpt(myHDF5Plugin, 'HDF1:')
+    # hdf1 = Cpt(myHDF5Plugin, 'HDF1:')
 
-    # # TODO: paramterize the paths, don't use absolutes
-    # # TODO: check and repair trainling slash
-    # hdf5 = Cpt(HDF5PluginWithFileStore,
-    #            suffix='HDF1:',
-    #            write_path_template='/direct/XF21ID1/image_files/',  # trailing slash!
-    #            read_path_template='/direct/XF21ID1/image_files/')
+simdet = MyDetector(AD_IOC_PREFIX)
 
-ad_prefix = '13SIM1:'
-simdet = MyDetector(ad_prefix)
-# assume sim detector is unconfigured, apply all config here
-simdet.cam.acquire_time.put(0.5)    # seconds
-simdet.cam.array_callbacks.put("Enable")
-simdet.cam.data_type.put("UInt8")
-simdet.cam.image_mode.put(0) # Single
-simdet.cam.num_exposures.put(1)
-simdet.cam.num_images.put(1)
-simdet.cam.shutter_mode.put("None")
-simdet.cam.trigger_mode.put("Internal")
-# specific to sim detector
-simdet.cam.sim_mode.put("LinearRamp")
+def setup_sim_detector(det):
+    # assume sim detector is unconfigured, apply all config here
+    cam = det.cam
 
-# simdet.hdf1.array_callbacks.put('Enable')
-# simdet.hdf1.auto_increment.put('Yes')
-# simdet.hdf1.auto_save.put('Yes')
-# simdet.hdf1.enable_callbacks.put('Enable')
-# simdet.hdf1.file_path.put(os.getcwd())  # TODO: get from EPICS PV
-# simdet.hdf1.file_name.put('tomoscan')   # TODO: get from EPICS PV
-# simdet.hdf1.file_template.put('%s%s_%5.5d.h5')
-# simdet.hdf1.file_write_mode.put('Single')
-# #simdet.hdf1.xml_layout_file.put('')
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Set up default metadata
+    cam.acquire_time.put(0.5)    # seconds
+    cam.array_callbacks.put("Enable")
+    cam.data_type.put("UInt8")
+    cam.image_mode.put(0) # Single
+    cam.num_exposures.put(1)
+    cam.num_images.put(1)
+    cam.shutter_mode.put("None")
+    cam.trigger_mode.put("Internal")
+    # specific to sim detector
+    cam.sim_mode.put("LinearRamp")
+    
+    if hasattr(det, 'hdf1'):
+        hdf = det.hdf1
+    
+        hdf.array_callbacks.put('Enable')
+        hdf.auto_increment.put('Yes')
+        hdf.auto_save.put('Yes')
+        hdf.enable_callbacks.put('Enable')
+        hdf.file_path.put(os.getcwd())  # TODO: get from EPICS PV
+        hdf.file_name.put('tomoscan')   # TODO: get from EPICS PV
+        hdf.file_template.put('%s%s_%5.5d.h5')
+        hdf.file_write_mode.put('Single')
+        # hdf.xml_layout_file.put('')    # won't work for empty strings, but why?
+
+
 RE.md['beamline_id'] = 'developer'
 RE.md['proposal_id'] = None
 RE.md['pid'] = os.getpid()
@@ -207,7 +178,9 @@ def print_scan_ids(name, start_doc):
     print("Transient Scan ID: {0}".format(start_doc['scan_id']))
     print("Persistent Unique Scan ID: '{0}'".format(start_doc['uid']))
 
+
 RE.subscribe('start', print_scan_ids)
+
 
 HOSTNAME = socket.gethostname() or 'localhost' 
 USERNAME = getpass.getuser() or 'synApps_xxx_user' 
@@ -230,10 +203,9 @@ class EpicsNotice(Device):
             self.msg_b.put(str(b))
 
 
-epics_string_notices = EpicsNotice('xxx:', name='messages')
+epics_string_notices = EpicsNotice(SYNAPPS_IOC_PREFIX, name='messages')
 epics_string_notices.post()
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
 #############################################################################
