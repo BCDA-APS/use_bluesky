42989a7b65ffe481b2356b2e5c923f191fdaf0c3
Pete Jemian <prjemian@gmail.com>
2017-04-19 17:32:04 -0500

    clean-up & organize imports, looks more like program code now

--- plans/standlone.py
+++ plans/standlone.py
@@ -15,10 +15,7 @@ SYNAPPS_IOC_PREFIX = 'xxx:'
 
 # these things must import and set first, then other imports
 import os
-import sys
 os.environ['EPICS_CA_MAX_ARRAY_BYTES'] = str(EPICS_CA_MAX_ARRAY_BYTES)
-
-import epics
 
 from ophyd import setup_ophyd
 setup_ophyd()
@@ -38,204 +35,91 @@ import logging
 import socket 
 import getpass 
 
+import filestore.fs
+import metadatastore.mds
+import databroker
 
-from metadatastore.mds import MDS       #, MDSRO
-from filestore.fs import FileStore      #, FileStoreRO
-from databroker import Broker
+import ophyd
+import bluesky, bluesky.global_state, bluesky.callbacks
+import suitcase
 
-from ophyd import Component as Cpt
-from ophyd import Device
-#from ophyd import Signal
-#from ophyd import PVPositioner
-#from ophyd import PVPositionerPC
-from ophyd import EpicsMotor
-from ophyd import EpicsSignal
-from ophyd import EpicsSignalRO
-from ophyd import EpicsScaler
-from ophyd import SingleTrigger
-from ophyd import AreaDetector
-from ophyd import SimDetector
-from ophyd import HDF5Plugin
-#from ophyd import TIFFPlugin
-
-# from ophyd.areadetector.filestore_mixins import FileStoreHDF5IterativeWrite
-# from ophyd.areadetector.filestore_mixins import FileStoreTIFFIterativeWrite
-# from ophyd.areadetector import ADComponent as ADCpt
-from ophyd.areadetector import EpicsSignalWithRBV
-# from ophyd.areadetector import ImagePlugin
-# from ophyd.areadetector import StatsPlugin
-# from ophyd.areadetector import DetectorBase
-# from ophyd.areadetector import ROIPlugin
-# from ophyd.areadetector import ProcessPlugin
-# from ophyd.areadetector import TransformPlugin
-
-from bluesky.global_state import gs
-from bluesky.callbacks import LiveTable
-
-# import suitcase.nexus
-
-#############################################################################
-
-
-class myHDF5Plugin(Device):
-    """custom handling of the HDF5 file writing plugin"""
-    # NOTE: ophyd.areadetector.HDF5Plugin needs testing with AD2.6, use Device for now
-  
-    array_callbacks = Cpt(EpicsSignalWithRBV, 'ArrayCallbacks')
-    enable_callbacks = Cpt(EpicsSignalWithRBV, 'EnableCallbacks')
-    auto_increment = Cpt(EpicsSignalWithRBV, 'AutoIncrement')
-    auto_save = Cpt(EpicsSignalWithRBV, 'AutoSave')
-    file_path = Cpt(EpicsSignalWithRBV, 'FilePath', string=True)
-    file_name = Cpt(EpicsSignalWithRBV, 'FileName', string=True)
-    file_number = Cpt(EpicsSignalWithRBV, 'FileNumber')
-    file_template = Cpt(EpicsSignalWithRBV, 'FileTemplate', string=True)
-    file_write_mode = Cpt(EpicsSignalWithRBV, 'FileWriteMode')
-    full_file_name = Cpt(EpicsSignalRO, 'FullFileName_RBV', string=True)
-    store_attributes = Cpt(EpicsSignalWithRBV, 'StoreAttr', string=True)
-    store_performance_data = Cpt(EpicsSignalWithRBV, 'StorePerform', string=True)
-    xml_layout_file = Cpt(EpicsSignalWithRBV, 'XMLFileName', string=True)
-
-
-class MyDetector(SingleTrigger, SimDetector):
-    """customize the sim detector handling"""
-    #  NOTE: ophyd.areadetector.ImagePlugin needs testing with AD2.6
-
-    no_op = None
-    # image1 = Cpt(ImagePlugin, 'image1:')
-    hdf1 = Cpt(myHDF5Plugin, 'HDF1:')
-
-
-def setup_sim_detector(det):
-    # assume sim detector is unconfigured, apply all config here
-    cam = det.cam
-
-    cam.acquire_time.put(0.5)    # seconds
-    cam.array_callbacks.put("Enable")
-    cam.data_type.put("UInt8")
-    cam.image_mode.put(0) # Single
-    cam.num_exposures.put(1)
-    cam.num_images.put(1)
-    cam.shutter_mode.put("None")
-    cam.trigger_mode.put("Internal")
-    # specific to sim detector
-    cam.sim_mode.put("LinearRamp")
-    
-    if hasattr(det, 'hdf1'):
-        hdf = det.hdf1
-    
-        hdf.array_callbacks.put('Enable')
-        hdf.auto_increment.put('Yes')
-        hdf.auto_save.put('Yes')
-        hdf.enable_callbacks.put('Enable')
-        hdf.file_path.put(os.getcwd())  # TODO: get from EPICS PV
-        hdf.file_name.put('tomoscan')   # TODO: get from EPICS PV
-        hdf.file_template.put('%s%s_%5.5d.h5')
-        hdf.file_write_mode.put('Single')
-        hdf.store_attributes.put("Yes")
-        hdf.store_performance_data.put("No")
-        # hdf.xml_layout_file.put('')    # won't work for empty strings, but why?
-
-
-def print_scan_ids(name, start_doc):
-    """callback that prints scan IDs at the start of each scan"""
-    print("Transient Scan ID: {0}".format(start_doc['scan_id']))
-    print("Persistent Unique Scan ID: '{0}'".format(start_doc['uid']))
-
-
-class EpicsNotice(Device):
-    '''
-    progress messages posted to a couple stringout PVs
-    '''
- 
-    msg_a = Cpt(EpicsSignal, 'userStringCalc1.AA')
-    msg_b = Cpt(EpicsSignal, 'userStringCalc1.BB')
-    
-    def post(self, a=None, b=None):
-        """write text to each/either PV"""
-        if a is not None:
-            self.msg_a.put(str(a)[:39])
-        if b is not None:
-            self.msg_b.put(str(b)[:39])
-
-
-#############################################################################
-
-logging.basicConfig()
-logger = logging.getLogger()
-logger.info('starting: ' + __file__)
-
-os.environ['MDS_HOST'] = MONGODB_HOST
-os.environ['MDS_PORT'] = '27017'
-os.environ['MDS_DATABASE'] = 'metadatastore-production-v1'
-os.environ['MDS_TIMEZONE'] = 'US/Central'
-os.environ['FS_HOST'] = os.environ['MDS_HOST']
-os.environ['FS_PORT'] = os.environ['MDS_PORT']
-os.environ['FS_DATABASE'] = 'filestore-production-v1'
-
-# Connect to metadatastore and filestore.
-mds_config = {'host': os.environ['MDS_HOST'],
-              'port': int(os.environ['MDS_PORT']),
-              'database': os.environ['MDS_DATABASE'],
-              'timezone': os.environ['MDS_TIMEZONE']}
-fs_config = {'host': os.environ['FS_HOST'],
-             'port': int(os.environ['FS_PORT']),
-             'database': os.environ['FS_DATABASE']}
-
-mds = MDS(mds_config)
-fs = FileStore(fs_config)
-db = Broker(mds, fs)
-
-RE = gs.RE  # convenience alias
-RE.subscribe('all', mds.insert)
-
-
-alpha = EpicsMotor(SYNAPPS_IOC_PREFIX + 'm1', name='alpha')
-beta = EpicsMotor(SYNAPPS_IOC_PREFIX + 'm2', name='beta')
-gamma = EpicsMotor(SYNAPPS_IOC_PREFIX + 'm3', name='gamma')
-x = EpicsMotor(SYNAPPS_IOC_PREFIX + 'm4', name='x')
-y = EpicsMotor(SYNAPPS_IOC_PREFIX + 'm5', name='y')
-z = EpicsMotor(SYNAPPS_IOC_PREFIX + 'm6', name='z')
-
-noisy = EpicsSignalRO(SYNAPPS_IOC_PREFIX + 'userCalc1', name='noisy')
-scaler = EpicsScaler(SYNAPPS_IOC_PREFIX + 'scaler1', name='scaler')
-
-
-simdet = MyDetector(AD_IOC_PREFIX)
-setup_sim_detector(simdet)
-
-
-RE.md['beamline_id'] = 'developer'
-RE.md['proposal_id'] = None
-RE.md['pid'] = os.getpid()
-
-
-RE.subscribe('start', print_scan_ids)
-
-
-HOSTNAME = socket.gethostname() or 'localhost' 
-USERNAME = getpass.getuser() or 'synApps_xxx_user' 
-RE.md['login_id'] = USERNAME + '@' + HOSTNAME
-
-
-epics_string_notices = EpicsNotice(SYNAPPS_IOC_PREFIX, name='messages')
-epics_string_notices.post()
-
+import my_devices
+import interlace_tomo
 
 
 #############################################################################
 
 if __name__ == '__main__':
-    import interlace_tomo
+
+    logging.basicConfig()
+    logger = logging.getLogger()
+    logger.info('starting: ' + __file__)
     
+    os.environ['MDS_HOST'] = MONGODB_HOST
+    os.environ['MDS_PORT'] = '27017'
+    os.environ['MDS_DATABASE'] = 'metadatastore-production-v1'
+    os.environ['MDS_TIMEZONE'] = 'US/Central'
+    os.environ['FS_HOST'] = os.environ['MDS_HOST']
+    os.environ['FS_PORT'] = os.environ['MDS_PORT']
+    os.environ['FS_DATABASE'] = 'filestore-production-v1'
+    
+    # Connect to metadatastore and filestore.
+    mds_config = {'host': os.environ['MDS_HOST'],
+                  'port': int(os.environ['MDS_PORT']),
+                  'database': os.environ['MDS_DATABASE'],
+                  'timezone': os.environ['MDS_TIMEZONE']}
+    fs_config = {'host': os.environ['FS_HOST'],
+                 'port': int(os.environ['FS_PORT']),
+                 'database': os.environ['FS_DATABASE']}
+    
+    mds = metadatastore.mds.MDS(mds_config)
+    fs = filestore.fs.FileStore(fs_config)
+    db = databroker.Broker(mds, fs)
+    
+    RE = bluesky.global_state.gs.RE  # convenience alias
+    RE.subscribe('all', mds.insert)
+    
+    
+    alpha = ophyd.EpicsMotor(SYNAPPS_IOC_PREFIX + 'm1', name='alpha')
+    beta = ophyd.EpicsMotor(SYNAPPS_IOC_PREFIX + 'm2', name='beta')
+    gamma = ophyd.EpicsMotor(SYNAPPS_IOC_PREFIX + 'm3', name='gamma')
+    x = ophyd.EpicsMotor(SYNAPPS_IOC_PREFIX + 'm4', name='x')
+    y = ophyd.EpicsMotor(SYNAPPS_IOC_PREFIX + 'm5', name='y')
+    z = ophyd.EpicsMotor(SYNAPPS_IOC_PREFIX + 'm6', name='z')
+    
+    noisy = ophyd.EpicsSignalRO(SYNAPPS_IOC_PREFIX + 'userCalc1', name='noisy')
+    scaler = ophyd.EpicsScaler(SYNAPPS_IOC_PREFIX + 'scaler1', name='scaler')
+    
+    
+    simdet = my_devices.MyDetector(AD_IOC_PREFIX)
+    my_devices.setup_sim_detector(simdet)
+    
+    
+    RE.md['beamline_id'] = 'developer'
+    RE.md['proposal_id'] = None
+    RE.md['pid'] = os.getpid()
+    
+    
+    RE.subscribe('start', my_devices.print_scan_ids)
+    
+    
+    HOSTNAME = socket.gethostname() or 'localhost' 
+    USERNAME = getpass.getuser() or 'synApps_xxx_user' 
+    RE.md['login_id'] = USERNAME + '@' + HOSTNAME
+    
+    
+    epics_string_notices = my_devices.EpicsNotice(SYNAPPS_IOC_PREFIX, name='messages')
+    epics_string_notices.post()
+
     tomo_callbacks = []
     detectors = [scaler,]
     prescan_checks = interlace_tomo.PreTomoScanChecks(alpha, source_intensity=None)
-    live_table = LiveTable([alpha, beta, scaler.time, scaler.channels.chan1, scaler.channels.chan2])
+    live_table = bluesky.callbacks.LiveTable([alpha, beta, scaler.time, scaler.channels.chan1, scaler.channels.chan2])
     epics_notifier = interlace_tomo.EPICSNotifierCallback(epics_string_notices)
     
     detectors = [simdet]
     live_table_signals = [alpha, beta]
-    live_table = LiveTable(live_table_signals)
+    live_table = bluesky.callbacks.LiveTable(live_table_signals)
 
     tomo_callbacks.append(prescan_checks)
     tomo_callbacks.append(live_table)
@@ -249,5 +133,6 @@ if __name__ == '__main__':
     
     # TODO: How to get frame file names into LiveTable?
 
-    tomo_plan = interlace_tomo.interlace_tomo_scan(detectors, alpha, 1.0, 1.8, 5, 8, snake=True, bisection=True)
+    simdet.cam.acquire_time.put(0.01)    # seconds
+    tomo_plan = interlace_tomo.interlace_tomo_scan(detectors, alpha, 0, 1.8, 7, 2, snake=True, bisection=True)
     RE(tomo_plan, tomo_callbacks, md=dict(developer=True))
