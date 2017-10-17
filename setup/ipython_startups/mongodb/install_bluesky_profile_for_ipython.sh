12eb4821ce315b8156c1a22c6435a1ae3341e022
prjemian <prjemian@gmail.com>
2017-10-16 21:05:33 -0500

    fixes #22

--- setup/ipython_startups/mongodb/install_bluesky_profile_for_ipython.sh
+++ setup/ipython_startups/mongodb/install_bluesky_profile_for_ipython.sh
@@ -22,6 +22,7 @@ FILE_LIST+=" 20-detectors.py"
 FILE_LIST+=" 60-metadata.py"
 FILE_LIST+=" 95_write_NeXus_when_stop.py"
 FILE_LIST+=" 99-describe_item.py"
+FILE_LIST+=" mongodb_config.yml"
 FILE_LIST+=" README.md"
 
 WWW_REPO=https://raw.githubusercontent.com/BCDA-APS/use_bluesky/master/setup/ipython_startups/mongodb
@@ -29,3 +30,7 @@ TARGET=${HOME}/.ipython/profile_$PROFILE
 for filename in ${FILE_LIST}; do
 	wget ${WWW_REPO}/${filename} -O ${TARGET}/startup/${filename}
 done
+
+mkdir -p ${HOME}/.config/databroker
+cd ${HOME}/.config/databroker
+ln -s ../../.ipython/profile_$PROFILE/startup/mongodb_config.yml ./
