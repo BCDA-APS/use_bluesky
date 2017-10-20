acfe5eefa7c31b256b4bb5fc6eb0d708a6593eb1
mdwyman <maxdwyman@gmail.com>
2017-10-20 11:11:04 -0500

    Edited use_bluesky to handle jupyter notebooks
    Signed-off-by: mdwyman <maxdwyman@gmail.com>

---
+++ setup/ipython_startups/mongodb/00-startup-jup.py
@@ -0,0 +1,31 @@
+print(__file__)
+
+# Make ophyd listen to pyepics.
+from ophyd import setup_ophyd
+setup_ophyd()
+
+from bluesky import RunEngine
+from bluesky.utils import get_history
+RE = RunEngine(get_history())
+
+# Import matplotlib and put it in interactive mode.
+import matplotlib.pyplot as plt
+
+# Make plots update live while scans run.
+from bluesky.utils import install_nb_kicker
+install_nb_kicker()
+
+# Optional: set any metadata that rarely changes. in 60-metadata.py
+
+# convenience imports
+from bluesky.callbacks import *
+from bluesky.plan_tools import print_summary
+import bluesky.plans as bp
+from time import sleep
+import numpy as np
+
+# Uncomment the following lines to turn on 
+# verbose messages for debugging.
+# import logging
+# ophyd.logger.setLevel(logging.DEBUG)
+# logging.basicConfig(level=logging.DEBUG)
