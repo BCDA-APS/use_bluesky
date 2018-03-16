06ea450993c162778fa5a5605734331712ac7c3b
prjemian <prjemian@gmail.com>
2018-03-16 16:50:35 -0500

    handle profile names better

--- bin/use_bluesky.sh
+++ bin/use_bluesky.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# wget https://raw.githubusercontent.com/BCDA-APS/use_bluesky/master/bin/use_bluesky.sh
+# https://raw.githubusercontent.com/BCDA-APS/use_bluesky/master/starters/use_bluesky.sh
 
 # start an interactive ipython session running BlueSky (from NSLS-II)
 #
@@ -26,6 +26,7 @@ if [ -d ~/Apps/BlueSky/bin ]; then
   export BLUESKY_BIN_DIR__INTERNAL_DEFAULT=~/Apps/BlueSky/bin
 elif [ -d /APSshare/anaconda3/BlueSky/bin ]; then
   export BLUESKY_BIN_DIR__INTERNAL_DEFAULT=/APSshare/anaconda3/BlueSky/bin
+  #export PYEPICS_LIBCA=/APSshare/anaconda3/BlueSky/lib/libca.so
 fi
 
 
@@ -37,36 +38,45 @@ export BLUESKY_PROFILE=${1:-$BLUESKY_PROFILE__INTERNAL_DEFAULT}
 
 
 general_startup () {
-	echo "BlueSky executable path: $BLUESKY_BIN_DIR"
-	$BLUESKY_BIN_DIR/python --version
-	$BLUESKY_BIN_DIR/conda --version
-	$BLUESKY_BIN_DIR/pip --version
+    echo "BlueSky executable path: $BLUESKY_BIN_DIR"
+    $BLUESKY_BIN_DIR/python --version
+    $BLUESKY_BIN_DIR/conda --version
+    $BLUESKY_BIN_DIR/pip --version
 }
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-export PATH=$BLUESKY_BIN_DIR:$PATH
+export PATH=${BLUESKY_BIN_DIR}:${PATH}
 
 case "$1" in
-	"")
-		general_startup
-		echo "starting ipython with profile: $BLUESKY_PROFILE"
-		$BLUESKY_BIN_DIR/ipython --profile=$BLUESKY_PROFILE
-		read -p "End of BlueSky console session - press return"
-	;;
-	"-jupyter")
-		general_startup
-		echo "Starting jupyter-notebook"
-		$BLUESKY_BIN_DIR/jupyter-notebook
-		read -p "End of BlueSky console session - press return"
-	;;
-	*)
-		echo "Incorrect arguments. Try either:"
-		echo "$ use_bluesky.sh "
-		echo "OR"
-		echo "$ use_bluesky.sh -jupyter"
-	;;
+    "")
+        general_startup
+        echo "starting ipython with profile: $BLUESKY_PROFILE"
+        $BLUESKY_BIN_DIR/ipython --profile=$BLUESKY_PROFILE
+        read -p "End of BlueSky console session - press return"
+    ;;
+    "-jupyter")
+        general_startup
+        echo "Starting jupyter-notebook"
+        $BLUESKY_BIN_DIR/jupyter-notebook
+        read -p "End of BlueSky console session - press return"
+    ;;
+    "-h" | --"help" | --"usage")
+        echo "usage (one of these):"
+        echo " use_bluesky.sh               # start with $BLUESKY_PROFILE__INTERNAL_DEFAULT as IPython profile"
+        echo " use_bluesky.sh profile_name  # start with profile_name as IPython profile"
+        echo " use_bluesky.sh -jupyter      # start a Jupyter notebook session"
+        echo " use_bluesky.sh -h            # this help"
+        echo " use_bluesky.sh --help        # this help"
+        echo " use_bluesky.sh --usage       # this help"
+    ;;
+    *)
+        general_startup
+        echo "starting ipython with profile: $BLUESKY_PROFILE"
+        $BLUESKY_BIN_DIR/ipython --profile=$BLUESKY_PROFILE
+        read -p "End of BlueSky console session - press return"
+    ;;
 esac
 
 
