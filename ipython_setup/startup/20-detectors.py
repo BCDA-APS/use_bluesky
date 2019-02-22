08ddbc0d8cc45f761c41057bcdf2a4b916ce28e2
Pete R Jemian <prjemian@users.noreply.github.com>
2019-02-22 11:38:15 -0600

    simplify

--- ipython_setup/startup/20-detectors.py
+++ ipython_setup/startup/20-detectors.py
@@ -1,14 +1,8 @@
 print(__file__)
 
-from ophyd import (EpicsScaler, EpicsSignal, EpicsSignalRO,
-                   Device, DeviceStatus)
-from ophyd import Component as Cpt
+scaler = ScalerCH('xxx:scaler1', name='scaler')
+# work around a bug in ScalerCH: just show the channels named in EPICS
+APS_devices.use_EPICS_scaler_channels(scaler)
 
-import time
-
-aps_current = EpicsSignalRO("S:SRcurrentAI", name="aps_current")
-
-## Beam Monitor Counts
-#bs_bm2 = EpicsSignalRO('BL14B:Det:BM2', name='bs_bm2')
+# demo: use this swait record to make a "noisy" detector signal
 noisy = EpicsSignalRO('xxx:userCalc1', name='noisy')
-scaler = EpicsScaler('xxx:scaler1', name='scaler')
