27317b52f64c835db75db2b711c487df88899f05
Pete R Jemian <prjemian@users.noreply.github.com>
2019-02-22 12:02:49 -0600

    sync with USAXS

--- ipython_setup/startup/60-metadata.py
+++ ipython_setup/startup/60-metadata.py
@@ -8,25 +8,15 @@ RE.md['beamline_id'] = 'developer__YOUR_BEAMLINE_HERE'
 RE.md['proposal_id'] = None
 RE.md['pid'] = os.getpid()
 
-# Add a callback that prints scan IDs at the start of each scan.
-
-def print_scan_ids(name, start_doc):
-    msg = "Transient Scan ID: "
-    msg += str(start_doc['scan_id'])
-    msg += " at "
-    msg += str(datetime.isoformat(datetime.now()))
-    print(msg)
-    print("Persistent Unique Scan ID: '{0}'".format(start_doc['uid']))
-
-callback_db['print_scan_ids'] = RE.subscribe(print_scan_ids, 'start')
-
-import socket 
-import getpass 
 HOSTNAME = socket.gethostname() or 'localhost' 
 USERNAME = getpass.getuser() or 'synApps_xxx_user' 
 RE.md['login_id'] = USERNAME + '@' + HOSTNAME
+RE.md['BLUESKY_VERSION'] = bluesky.__version__
+RE.md['OPHYD_VERSION'] = ophyd.__version__
+from apstools import __version__ as apstools_version
+RE.md['APSTOOLS_VERSION'] = apstools_version
+del apstools_version
 
-import os
-for key, value in os.environ.items():
-    if key.startswith("EPICS"):
-        RE.md[key] = value
+print("Metadata dictionary:")
+for k, v in sorted(RE.md.items()):
+    print("RE.md['%s']" % k, "=", v)
