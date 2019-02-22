6a0db8505f36d0958a3cb449f8ac532022a86380
Pete R Jemian <prjemian@users.noreply.github.com>
2019-02-22 11:29:03 -0600

    renaming

---
+++ ipython_setup/startup/15-motors.py
@@ -0,0 +1,12 @@
+print(__file__)
+
+"""the motors"""
+
+m1 = EpicsMotor('xxx:m1', name='m1')
+m2 = EpicsMotor('xxx:m2', name='m2')
+m3 = EpicsMotor('xxx:m3', name='m3')
+m4 = EpicsMotor('xxx:m4', name='m4')
+m5 = EpicsMotor('xxx:m5', name='m5')
+m6 = EpicsMotor('xxx:m6', name='m6')
+m7 = EpicsMotor('xxx:m7', name='m7')
+m8 = EpicsMotor('xxx:m8', name='m8')
