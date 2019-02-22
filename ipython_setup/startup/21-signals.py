71e1ff7e2cf96d08a2bfd1939b6b6fb2f5554cf3
Pete R Jemian <prjemian@users.noreply.github.com>
2019-02-22 11:41:47 -0600

    Create 21-signals.py

---
+++ ipython_setup/startup/21-signals.py
@@ -0,0 +1,10 @@
+print(__file__)
+
+"""signals"""
+
+# APS only:
+# aps = APS_devices.ApsMachineParametersDevice(name="aps")
+# sd.baseline.append(aps)
+
+# undulator = APS_devices.ApsUndulatorDual("ID45", name="undulator")
+# sd.baseline.append(undulator)
