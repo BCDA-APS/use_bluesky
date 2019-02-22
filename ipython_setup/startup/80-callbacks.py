a05b46b961f3a42d6e23e123abccf735114e3319
Pete R Jemian <prjemian@users.noreply.github.com>
2019-02-22 12:19:00 -0600

    sync with USAXS

--- ipython_setup/startup/80-callbacks.py
+++ ipython_setup/startup/80-callbacks.py
@@ -2,15 +2,15 @@ print(__file__)
 
 # custom callbacks
 
-import APS_BlueSky_tools.callbacks
-import APS_BlueSky_tools.filewriters
-
-# collect last scan's documents into doc_collector.documents
-# doc_collector = APS_BlueSky_tools.callbacks.DocumentCollectorCallback()
-# callback_db['doc_collector'] = RE.subscribe(doc_collector.receiver)
-
 # write scans to SPEC data file
-specwriter = APS_BlueSky_tools.filewriters.SpecWriterCallback()
-# make the SPEC file in /tmp (assumes OS is Linux)
-specwriter.newfile(os.path.join("/tmp", specwriter.spec_filename))
+specwriter = APS_filewriters.SpecWriterCallback()
+#_path = "/tmp"      # make the SPEC file in /tmp (assumes OS is Linux)
+_path = os.getcwd() # make the SPEC file in current working directory (assumes is writable)
+specwriter.newfile(os.path.join(_path, specwriter.spec_filename))
 callback_db['specwriter'] = RE.subscribe(specwriter.receiver)
+print(f"""
+  writing to SPEC file: {specwriter.spec_filename}
+  >>>>   Using default SPEC file name   <<<<
+  file will be created when bluesky ends its next scan
+  to change SPEC file, use command:   newSpecFile('title')
+""")
