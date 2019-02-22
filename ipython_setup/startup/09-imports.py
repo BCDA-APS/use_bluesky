2c1579fbe0a05adeb589a31c3587f19bd8a468fb
Pete R Jemian <prjemian@users.noreply.github.com>
2019-02-22 11:24:35 -0600

    bring all import statements together

---
+++ ipython_setup/startup/09-imports.py
@@ -0,0 +1,31 @@
+print(__file__)
+
+"""gather all the imports here"""
+
+
+import datetime
+from enum import Enum
+import getpass 
+import itertools
+import os
+import socket 
+import time
+import uuid
+
+from ophyd import Component, Device, DeviceStatus, Signal
+from ophyd import EpicsMotor, MotorBundle
+from ophyd import EpicsSignal, EpicsSignalRO, EpicsSignalWithRBV
+from ophyd.scaler import ScalerCH, ScalerChannel
+from ophyd.sim import SynSignal
+
+import apstools.callbacks as APS_callbacks
+import apstools.devices as APS_devices
+import apstools.filewriters as APS_filewriters
+import apstools.plans as APS_plans
+import apstools.synApps_ophyd as APS_synApps_ophyd
+import apstools.suspenders as APS_suspenders
+import apstools.utils as APS_utils
+
+# import specific methods by name, we need to customize them sometimes
+from apstools.devices import SimulatedApsPssShutterWithStatus
+from apstools.filewriters import SpecWriterCallback, spec_comment
