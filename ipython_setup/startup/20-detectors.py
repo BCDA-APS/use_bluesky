b5600f492f548c86f65cdc8ef9f90caf083980e3
Peter Jemian <jemian@wow.aps.anl.gov>
2019-07-09 13:33:21 -0500

    MNT #38 wait for scaler to connect

--- ipython_setup/startup/20-detectors.py
+++ ipython_setup/startup/20-detectors.py
@@ -3,8 +3,9 @@ print(__file__)
 """detectors (area detectors handled separately)"""
 
 scaler = ScalerCH('xxx:scaler1', name='scaler')
-# work around a bug in ScalerCH: just show the channels named in EPICS
-APS_devices.use_EPICS_scaler_channels(scaler)
+while not scaler.connected:
+	time.sleep(0.1)
+scaler.select_channels(None)
 
 # demo: use this swait record to make a "noisy" detector signal
 noisy = EpicsSignalRO('xxx:userCalc1', name='noisy')
