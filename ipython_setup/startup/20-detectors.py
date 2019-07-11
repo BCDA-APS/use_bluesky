7585535787b9c826bb6bb8e72c45aa746d5a5647
Pete R Jemian <prjemian@users.noreply.github.com>
2019-07-11 14:03:33 -0500

    wrap with try..except instead

--- ipython_setup/startup/20-detectors.py
+++ ipython_setup/startup/20-detectors.py
@@ -2,10 +2,13 @@ logger.info(__file__)
 
 """detectors (area detectors handled separately)"""
 
-# scaler = ScalerCH('xxx:scaler1', name='scaler')
-# while not scaler.connected:
-# 	time.sleep(0.1)
-# scaler.select_channels(None)
+try:
+    scaler = ScalerCH('xxx:scaler1', name='scaler')
+    while not scaler.connected:
+        time.sleep(0.1)
+    scaler.select_channels(None)
 
-# # demo: use this swait record to make a "noisy" detector signal
-# noisy = EpicsSignalRO('xxx:userCalc1', name='noisy')
+    # demo: use this swait record to make a "noisy" detector signal
+    noisy = EpicsSignalRO('xxx:userCalc1', name='noisy')
+except TimeoutError:
+    logger.warning(f"Could not connect 20-detectors.py")
