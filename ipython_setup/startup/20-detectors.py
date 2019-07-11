5acadfca55fdbab9de4f281afea76900717f04ae
Pete R Jemian <prjemian@users.noreply.github.com>
2019-07-11 13:58:38 -0500

    comment out these fine examples

--- ipython_setup/startup/20-detectors.py
+++ ipython_setup/startup/20-detectors.py
@@ -2,10 +2,10 @@ logger.info(__file__)
 
 """detectors (area detectors handled separately)"""
 
-scaler = ScalerCH('xxx:scaler1', name='scaler')
-while not scaler.connected:
-	time.sleep(0.1)
-scaler.select_channels(None)
+# scaler = ScalerCH('xxx:scaler1', name='scaler')
+# while not scaler.connected:
+# 	time.sleep(0.1)
+# scaler.select_channels(None)
 
-# demo: use this swait record to make a "noisy" detector signal
-noisy = EpicsSignalRO('xxx:userCalc1', name='noisy')
+# # demo: use this swait record to make a "noisy" detector signal
+# noisy = EpicsSignalRO('xxx:userCalc1', name='noisy')
