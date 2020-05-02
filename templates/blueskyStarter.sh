f29f4051b0eab89bd95dd511ef32d07b95a65ac1
Pete Jemian <prjemian@gmail.com>
2020-05-02 18:02:18 -0500

    ENH #49 starter script

---
+++ templates/blueskyStarter.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export CONDA_ACTIVATE=${HOME}/Apps/anaconda/bin/activate
+export CONDA_ENVIRONMENT=bluesky_2020_5
+# bluesky
+
+export IPYTHON_PROFILE=bluesky
+export IPYTHONDIR=~/.ipython
+
+export OPTIONS=""
+export OPTIONS="${OPTIONS} --profile=${IPYTHON_PROFILE}"
+export OPTIONS="${OPTIONS} --ipython-dir=${IPYTHONDIR}"
+export OPTIONS="${OPTIONS} --IPCompleter.use_jedi=False"
+export OPTIONS="${OPTIONS} --InteractiveShellApp.hide_initial_ns=False"
+
+source ${CONDA_ACTIVATE} ${CONDA_ENVIRONMENT}
+ipython ${OPTIONS}
