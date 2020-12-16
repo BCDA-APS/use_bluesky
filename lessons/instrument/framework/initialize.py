1540d8825939434169247405989003760da8822a
Pete R. Jemian <prjemian@gmail.com>
2020-12-16 13:12:02 -0600

    MNT #77 for ophyd 1.6+

--- lessons/instrument/framework/initialize.py
+++ lessons/instrument/framework/initialize.py
@@ -16,7 +16,28 @@ from ..session_logs import logger
 logger.info(__file__)
 
 from bluesky import RunEngine
-# from bluesky.utils import PersistentDict
+from bluesky import SupplementalData
+from bluesky.callbacks.best_effort import BestEffortCallback
+from bluesky.callbacks.broker import verify_files_saved
+from bluesky.magics import BlueskyMagics
+from bluesky.utils import PersistentDict
+from bluesky.utils import ProgressBarManager
+from bluesky.utils import ts_msg_hook
+from IPython import get_ipython
+import databroker
+import ophyd
+import warnings
+
+# convenience imports
+# from bluesky.callbacks import *
+# from bluesky.callbacks.broker import *
+# from bluesky.simulators import *
+from bluesky.simulators import summarize_plan
+import bluesky.plan_stubs as bps
+import bluesky.plans as bp
+import bluesky.preprocessors as bpp
+import numpy as np
+
 
 # Set up a RunEngine and use metadata backed PersistentDict
 RE = RunEngine({})
@@ -29,7 +50,6 @@ RE.md = {}      # empty dict for lessons
 callback_db = {}
 
 # Set up a Broker.
-import databroker
 db = databroker.temp().v1
 
 # Subscribe metadatastore to documents.
@@ -37,22 +57,17 @@ db = databroker.temp().v1
 callback_db['db'] = RE.subscribe(db.insert)
 
 # Set up SupplementalData.
-from bluesky import SupplementalData
 sd = SupplementalData()
 RE.preprocessors.append(sd)
 
 # Add a progress bar.
-from bluesky.utils import ProgressBarManager
 pbar_manager = ProgressBarManager()
 RE.waiting_hook = pbar_manager
 
 # Register bluesky IPython magics.
-from IPython import get_ipython
-from bluesky.magics import BlueskyMagics
 get_ipython().register_magics(BlueskyMagics)
 
 # Set up the BestEffortCallback.
-from bluesky.callbacks.best_effort import BestEffortCallback
 bec = BestEffortCallback()
 callback_db['bec'] = RE.subscribe(bec)
 peaks = bec.peaks  # just an alias, for less typing
@@ -60,27 +75,31 @@ bec.disable_baseline()
 
 # At the end of every run, verify that files were saved and
 # print a confirmation message.
-from bluesky.callbacks.broker import verify_files_saved
 # callback_db['post_run_verify'] = RE.subscribe(post_run(verify_files_saved), 'stop')
 
-# convenience imports
-# from bluesky.callbacks import *
-# from bluesky.callbacks.broker import *
-# from bluesky.simulators import *
-import bluesky.plans as bp
-import bluesky.plan_stubs as bps
-import bluesky.preprocessors as bpp
-import numpy as np
 
-# Uncomment the following lines to turn on 
+# Uncomment the following lines to turn on
 # verbose messages for debugging.
 # ophyd.logger.setLevel(logging.DEBUG)
 
 # diagnostics
-from bluesky.utils import ts_msg_hook
 #RE.msg_hook = ts_msg_hook
-from bluesky.simulators import summarize_plan
 
 # set default timeout for all EpicsSignal connections & communications
-import ophyd
-ophyd.EpicsSignalBase.set_default_timeout(timeout=10, connection_timeout=5)
+if hasattr(ophyd, ""):
+    ophyd.EpicsSignalBase.set_defaults(
+        auto_monitor=True,
+        timeout=60,
+        write_timeout=60,
+        connection_timeout=5,
+    )
+else:
+    warnings.warn(
+        f"Version of ophyd {ophyd.__version__} is old,"
+        " upgrade to 1.6.0+ to get set_defaults() method."
+        "  Cannot set default: auto_monitor=True to use CA cache."
+    )
+    ophyd.EpicsSignalBase.set_default_timeout(
+        timeout=10, 
+        connection_timeout=5
+    )
