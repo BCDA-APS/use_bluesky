4371b484d2546f54c9244ba239cd7fa373c3422b
Pete R Jemian <prjemian@gmail.com>
2021-10-13 17:05:37 -0500

    FIX #107

--- python_installation/for_deletion/install_startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "There is a newer version of these instructions:"
-echo "https://github.com/BCDA-APS/use_bluesky/blob/main/install/README.md#install-instrument-package"
-echo ""
-echo "This script has a new version (2021-02-21):"
-echo "https://github.com/BCDA-APS/use_bluesky/raw/main/install/install_startup.sh"
